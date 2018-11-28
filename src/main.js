59abc714973dbd68e38b0cb89952672d3281ca29
jeffbeagley <jeffbeagley@gmail.com>
2018-11-27 18:34:22 -0600

    added vue-notification

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import './assets/css/transitions.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import Notifications from 'vue-notification'
 
 library.add(faCheck)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -19,6 +20,7 @@ Vue.config.productionTip = false
 
 Vue.config.devtools = true
 Vue.use(BootstrapVue)
+Vue.use(Notifications)
 
 router.beforeEach((to, from, next) => {
     document.title = to.meta.title
